0ac0856be5f50dd8fa44fe1b0e016f7d95683a63
vamshikrishna5038@gmail.com <vamshikrishna5038@gmail.com>
2022-11-21 02:50:18 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ResumeComponent } from './resume/resume.component';
+import { WorkComponent } from './work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'work',component:WorkComponent},
+  {path:'resume',component:ResumeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingcomponents=[HomeComponent,
+  AboutComponent,
+  NavbarComponent,
+  ContactComponent,WorkComponent,
+ResumeComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingcomponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+   routingcomponents,
+  
+ 
+
   ],
   imports: [
     BrowserModule,
